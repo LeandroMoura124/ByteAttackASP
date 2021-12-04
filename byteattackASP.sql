3e9914a4b8e5fb202ee0866e02d6097d1f249544
LeandroMoura124 <leofcb334@gmail.com>
2021-12-04 11:54:28 -0300

    commit do projeto

--- byteattackASP.sql
+++ byteattackASP.sql
@@ -68,7 +68,7 @@ select * from cliente;
 truncate cliente;
 
 
-insert into Especialista values('Leandro')
+insert into Especialista values('Leandro M', '2004-10-11', '49931503858', 'leokurt4@gmail.com', '11985803670', '1');
 
 
 
